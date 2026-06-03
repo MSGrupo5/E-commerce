f7a64e7840f6caaf998873299166ed258c5b3f4b
Mauricio <mauricionair2004@gmail.com>
2026-06-03 12:17:12 -0300

    feat: MSGRUP-39 - Asignar rol por defecto cliente automaticamente

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'cliente', // Agregado para MSGRUP-39
         ]);
 
         event(new Registered($user));
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role',
+        'role',// Agregado para MSGRUP-39
     ];
 
     /**
