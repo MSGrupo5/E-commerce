08a05ec5d5cdddd2295a07df230b915b9e151688
Mauricio <mauricionair2004@gmail.com>
2026-06-04 01:10:54 -0300

    db: MSGRUP-27 - Hacer nullable el campo apellido para compatibilidad con tests

--- database/migrations/2026_06_03_141249_add_apellido_to_users_table.php
+++ database/migrations/2026_06_03_141249_add_apellido_to_users_table.php
@@ -9,12 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+        public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('apellido')->after('name');
+            $table->string('apellido')->nullable()->after('name');
         });
     }
+
 
     /**
      * Reverse the migrations.
