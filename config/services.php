1a2aa5a15bf954dd3e042aedba7f479885b1d41b
Tomas Centurion <tomascenturion895@gmail.com>
2026-06-19 01:49:35 -0300

    Implementacion de filtro para comentarios ofensivos

--- config/services.php
+++ config/services.php
@@ -35,4 +35,9 @@ return [
         ],
     ],
 
+    'azure_content_safety' => [
+        'endpoint' => env('AZURE_CONTENT_SAFETY_ENDPOINT'), // ej: mi-recurso (sin https://)
+        'key'      => env('AZURE_CONTENT_SAFETY_KEY'),
+    ],
+
 ];
