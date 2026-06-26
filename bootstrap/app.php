7cb74c75378185d3ca2c0da8b3c4948f6a99b6f4
ramixPixel <ramirocorrales2006@gmail.com>
2026-06-26 00:28:31 -0300

    arregle el tema de cloudflared (#95)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => EnsureUserIsAdmin::class,
             'redirect.if.admin' => RedirectIfAdmin::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (PostTooLargeException $e) {
